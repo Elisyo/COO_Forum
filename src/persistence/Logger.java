a72a7b941631c6cb1824c2ac38cafb7ec5ffa67a
guilbertf <florian.guilbert@etudiant.univ-lille1.fr>
2016-10-20 17:14:52 +0200

    mise en place de log pour des cas d'erreur

---
+++ src/persistence/Logger.java
@@ -0,0 +1,10 @@
+package persistence;
+
+/**
+ * Interface gérant la mise en place d'un fichier de logs.
+ * Ce fichier servira à récupérer toutes les erreurs de l'application
+ * et les placera dans un fichier spécial.
+ */
+public interface Logger {
+
+}
--- src/persistence/UserMapper.java
+++ src/persistence/UserMapper.java
@@ -1,20 +1,41 @@
 package persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import domain.User;
 
 public class UserMapper {
 
 	static UserMapper inst;
+	private static Connection c;
+	int ID;
+
 	public static UserMapper getInstance(){
+		c = MySqlConnection.c;
 		if (inst == null)
 			inst = new UserMapper();
 		return inst;
 	}
-	User findById(int id) {
+	
+	User findById(int idUser) {
 		// methode bidon pour tester (en vrai: on recupere depuis la B.D.D.)
-		User p = new User(1,"pseudo", "motDePasse", "admin");
-		p.add(UnitOfWork.getInstance());
-		return p;
+		String req = "SELECT nom FROM user WHERE idUser=?";
+		PreparedStatement ps;
+		try {
+			ps = c.prepareStatement(req);
+			ps.setInt(1, idUser);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+		} catch (Exception e) {
+			System.out.println("User don't exist !");
+			e.printStackTrace();
+		}
+		User u = new User(1,"pseudo", "motDePasse", "admin");
+		u.add(UnitOfWork.getInstance());
+		return u;
 	}
 	public void update(User u) {
 		System.out.println("On met a jour l'utilisateur dans la base de donnees.");
